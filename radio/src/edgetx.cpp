5f5ab68c256f7ebf02b15e4c36f747519e3f8809
benjinne <benjinne@gmail.com>
2025-10-16 13:05:01 -0700

    modalai always boot to telemetry screen

--- radio/src/edgetx.cpp
+++ radio/src/edgetx.cpp
@@ -1389,7 +1389,11 @@ void edgeTxInit()
   ViewMain::instance();
 #elif defined(GUI)
   // TODO add a function for this (duplicated)
+#if defined(PCBMODALAI) && defined(DEFAULT_VIEW_TELEMETRY)
+  menuHandlers[0] = menuViewTelemetry;
+#else
   menuHandlers[0] = menuMainView;
+#endif
   menuHandlers[1] = menuModelSelect;
 #endif
 
